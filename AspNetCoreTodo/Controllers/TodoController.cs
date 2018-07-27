691fac7d5cf0dd027796facd781f55924c13e9e3
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-07-27 16:03:47 -0400

    Added 'My To-dos' to NavBar

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -1,19 +1,32 @@
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreTodo.Controllers
 {
     public class TodoController : Controller
     {
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
         // Actions go here
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             // Get to-do items from database
+            var items = await _todoItemService.GetIncompleteItemsAsync();
 
             // Put items into a model
+            var model = new TodoViewModel()
+            {
+                Items = items
+            };
 
             // Render view using the model
-
-            return null;
+            return View(model);
 
         }
     }
