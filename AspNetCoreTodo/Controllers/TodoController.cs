0bc370421af864504f70a26785a010259faedb0e
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-07-27 14:55:07 -0400

    Customized the stylesheet

---
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+        // Actions go here
+        public IActionResult Index()
+        {
+            // Get to-do items from database
+
+            // Put items into a model
+
+            // Render view using the model
+
+            return null;
+
+        }
+    }
+}
